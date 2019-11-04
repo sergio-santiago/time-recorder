c0562c70ceb36a26494af0159f755dbfe24815ed
Sergio Santiago <sersanhen@gmail.com>
2019-11-04 17:37:27 +0100

    change password

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
@@ -35,5 +38,39 @@ class HomeController extends Controller
         }
 
         return redirect()->route('time-record');
+    }
+
+    public function renderChangePasswordForm()
+    {
+        return view('auth.passwords.change_password');
+    }
+
+    public function processChangePasswordForm(Request $request)
+    {
+        $user = Auth::user();
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'old_password' => ['required', 'string'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+            ]
+        );
+
+        if ($validator->fails()) {
+            return view('auth.passwords.change_password')
+                ->withErrors($validator);
+        }
+
+        if (Hash::check($request->old_password, $user->password)) {
+            $user->password = Hash::make($request->password);
+            $user->save();
+            $request->session()->flash('alert-success', 'Password changed successfully!');
+        } else {
+            $validator->errors()->add('old_password', 'Old password incorrect');
+        }
+
+        return view('auth.passwords.change_password')
+            ->withErrors($validator);
     }
 }
---
+++ resources/views/auth/passwords/change_password.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Change Password') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('render-change-password-form') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="old_password"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Old Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="old_password" type="password"
+                                           class="form-control @error('old_password') is-invalid @enderror"
+                                           name="old_password" required autocomplete="old_password" autofocus>
+
+                                    @error('old_password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('New Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password"
+                                           class="form-control @error('password') is-invalid @enderror" name="password"
+                                           required autocomplete="new-password">
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password-confirm"
+                                       class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control"
+                                           name="password_confirmation" required autocomplete="new-password">
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Change Password') }}
+                                    </button>
+                                </div>
+                            </div>
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

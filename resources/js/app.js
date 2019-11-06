d9bbd4a9792ca41b4dd5c517eb3821e59822d3b7
Sergio Santiago <sersanhen@gmail.com>
2019-11-06 19:47:43 +0100

    Assets managing refactor

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,7 +30,3 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
-
-require('./switchRoleModal');
-require('./removeUserModal');
-require('./testTime');
--- resources/js/testTime.js
+++
@@ -1,1 +0,0 @@
-let input = document.getElementById('test-time');

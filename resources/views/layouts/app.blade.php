017a3429117d51da5b9869abede26ab2e250225f
Sergio Santiago <sersanhen@gmail.com>
2019-11-06 19:47:43 +0100

    Assets managing refactor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,15 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+@yield('js_adhoc')
 
-    <!-- Fonts -->
+<!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css_adhoc')
 </head>
 <body>
 <div id="app">
